44415c4e431d9cc73b620422ff85ebd8b1a9bcf1
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-05-17 00:32:45 +0600

    Class Attribute and __dict__ attribute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Item:
+    pay_rate = 0.8 # Class Attribute. For a 20% discount.
+
     # Any methods starting and ending with double underscore is called Magic/Dunder methods
     def __init__(self, name : str, price : float , quantity=0) -> None:
         # We specified the acceptable data types for each received arguments.
@@ -18,21 +20,56 @@ class Item:
 
     def calculate_total_price(self):
         return f'Total price for {self.name} is: {self.price * self.quantity}' 
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate # Here, we can also write Item.pay_rate. But it won't work if we want to change the pay_rate for any specific instance of the Item Class. So, it's a good idea to access pay_rate from instance level. So that we can reassign it.
 
 
 # Instance of the Item class
 item1 = Item("Phone", 100, 3)
 # Another Instance of the Item class
-item2 = Item("Laptop", 5000, 5)
+item2 = Item("Laptop", 1000, 5)
 
-print(item1.name)
-print(item1.price)
-print(item1.quantity)
+print(Item.pay_rate) # 0.8
 
-print(item2.name)
-print(item2.price)
-print(item2.quantity)
+# At first, Instance item1 tries to bring the attribute from instance level, i.e. from the __init__ method. If its not there then it brings from the class level.
+print(item1.pay_rate) # 0.8 
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+
+# At first, Instance item2 tries to bring the attribute from instance level, i.e. from the __init__ method. If its not there then it brings from the class level.
+print(item2.pay_rate) # 0.8
+
+
+
+# The __dict__ in Python represents a dictionary or any mapping object that is used to store the attributes of the object. They are also known as mappingproxy objects. To put it simply, every object in Python has an attribute that is denoted by __dict__.
+# __dict__ is also called magic attribute, not magic method.
+
+# All attributes for Class level
+print(Item.__dict__) # gives pay_rate along with many other things.
+
+# All attributes for Instance level
+print(item1.__dict__) # {'name': 'Phone', 'price': 100, 'quantity': 3}
+
+
+item1.apply_discount()
+print(item1.price)  # 80.0
+
+
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price) # 700.0
+
+# These are not necessary for this topic. 
+# print(item1.name)
+# print(item1.price)
+# print(item1.quantity)
+
+# print(item2.name)
+# print(item2.price)
+# print(item2.quantity)
+
+# print(item1.calculate_total_price())
+# print(item2.calculate_total_price())
+
+
 
