26d1be875d852e1d631f5784040ce18bdac8316a
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-05-17 01:51:48 +0600

    __repr__ method and what happens without it

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 class Item:
     pay_rate = 0.8 # Class Attribute. For a 20% discount.
-
-    # Any methods starting and ending with double underscore is called Magic/Dunder methods
+    all = []
     def __init__(self, name : str, price : float , quantity=0) -> None:
-        # We specified the acceptable data types for each received arguments.
-        # name accepts string type.
-        # price accepts float type but also int type.
-        # quantity accepts float and int by default for assigning the default value 0.
 
         # Run Validation for received arguments
         assert price >= 0, f"Price: {price} cannot be less than 0"
@@ -18,58 +13,37 @@ class Item:
         self.price = price # Assigning dynamic attribute
         self.quantity = quantity # Assigning dynamic attribute
 
+        # Actions to execute
+        Item.all.append(self)
+    
+    
+    # This method is for "representing" the Item class according to our desired way. Without this method print(Item.all) would show the memory address of each instance object.
+    def __repr__(self) -> str:
+        return f"Item('{self.name}, {self.price}, {self.quantity}')"
+
     def calculate_total_price(self):
         return f'Total price for {self.name} is: {self.price * self.quantity}' 
     
     def apply_discount(self):
-        self.price = self.price * self.pay_rate # Here, we can also write Item.pay_rate. But it won't work if we want to change the pay_rate for any specific instance of the Item Class. So, it's a good idea to access pay_rate from instance level. So that we can reassign it.
+        self.price = self.price * self.pay_rate 
+    
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
 
-# Instance of the Item class
-item1 = Item("Phone", 100, 3)
-# Another Instance of the Item class
-item2 = Item("Laptop", 1000, 5)
+print(Item.all)
+# Output: [Item('Phone, 100, 1'), Item('Laptop, 1000, 3'), Item('Cable, 10, 5'), Item('Mouse, 50, 5'), Item('Keyboard, 75, 5')]
 
-print(Item.pay_rate) # 0.8
-
-# At first, Instance item1 tries to bring the attribute from instance level, i.e. from the __init__ method. If its not there then it brings from the class level.
-print(item1.pay_rate) # 0.8 
-
-
-# At first, Instance item2 tries to bring the attribute from instance level, i.e. from the __init__ method. If its not there then it brings from the class level.
-print(item2.pay_rate) # 0.8
-
-
-
-# The __dict__ in Python represents a dictionary or any mapping object that is used to store the attributes of the object. They are also known as mappingproxy objects. To put it simply, every object in Python has an attribute that is denoted by __dict__.
-# __dict__ is also called magic attribute, not magic method.
-
-# All attributes for Class level
-print(Item.__dict__) # gives pay_rate along with many other things.
-
-# All attributes for Instance level
-print(item1.__dict__) # {'name': 'Phone', 'price': 100, 'quantity': 3}
-
-
-item1.apply_discount()
-print(item1.price)  # 80.0
-
-
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price) # 700.0
-
-# These are not necessary for this topic. 
-# print(item1.name)
-# print(item1.price)
-# print(item1.quantity)
-
-# print(item2.name)
-# print(item2.price)
-# print(item2.quantity)
-
-# print(item1.calculate_total_price())
-# print(item2.calculate_total_price())
-
-
+for instance in Item.all:
+    print(instance)
+# Output: 
+# Item('Phone, 100, 1')
+# Item('Laptop, 1000, 3')
+# Item('Cable, 10, 5')
+# Item('Mouse, 50, 5')
+# Item('Keyboard, 75, 5')
 
