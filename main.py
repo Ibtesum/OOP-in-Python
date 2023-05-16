f1d4cfc3df10510ae66b7918f43d9e4c7527678a
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-05-16 19:01:41 +0600

    small bug fix

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
 print(item1.calculate_total_price(item1.price, item1.quantity))
+print(type(item1))
 
 
 item2 = Item()
 item2.name = "Laptop"
 item2.price = 1000
 item2.quantity = 3
-print(item2.calculate_total_price(item2.price * item2))
+print(item2.calculate_total_price(item2.price, item2.quantity))
 
 
