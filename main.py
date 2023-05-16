feb3eab3c78c6ac46ea6832281a8d760fb81c9d6
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-05-16 20:29:21 +0600

    AssertError and Type Checking in receveived arguments

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 class Item:
     # Any methods starting and ending with double underscore is called Magic/Dunder methods
-    def __init__(self, name, price , quantity=0) -> None: # __init__ returns None type
+    def __init__(self, name : str, price : float , quantity=0) -> None:
+        # We specified the acceptable data types for each received arguments.
+        # name accepts string type.
+        # price accepts float type but also int type.
+        # quantity accepts float and int by default for assigning the default value 0.
+
+        # Run Validation for received arguments
+        assert price >= 0, f"Price: {price} cannot be less than 0"
+        assert quantity >= 0, f"Quantity: {quantity} cannot be less than 0"
+        # The above code creates AssertionError with the message written in formatted string if the instance doesn't fullfill the requirements.
+
+        # Assign Values To self Object
         self.name = name # Assigning dynamic attribute
         self.price = price # Assigning dynamic attribute
         self.quantity = quantity # Assigning dynamic attribute
+
     def calculate_total_price(self):
         return f'Total price for {self.name} is: {self.price * self.quantity}' 
+
 
 # Instance of the Item class
 item1 = Item("Phone", 100, 3)
